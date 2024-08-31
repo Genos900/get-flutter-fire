37413ae43d11f7c45ac5c64b04bb6a869e36906a
Genos900 <abhishekbrahma2003@gmail.com>
2024-09-01 00:07:54 +0530

    Abhishek Brahma 1032211046 MIT WPU

---
+++ flutter_spotify_ui-starter_project/flutter_spotify_ui project/lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+
+import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/data/data.dart';
+import 'package:flutter_spotify_ui/widgets/side_menu.dart';
+
+import 'screens/playlist_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isMacOS || Platform.isLinux || Platform.isWindows)) {
+    await DesktopWindow.setMinWindowSize(const Size(600, 800));
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Spotify UI',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        primaryColor: Colors.black,
+        accentColor: const Color.fromARGB(255, 29, 138, 67),
+        iconTheme: const IconThemeData().copyWith(color: Colors.white),
+        fontFamily: 'Montserrat',
+        textTheme: TextTheme(
+          headline2: const TextStyle(
+            color: Colors.white,
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            fontSize: 12.0,
+            color: Colors.grey[300],
+            fontWeight: FontWeight.w500,
+            letterSpacing: 2.0,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.grey[300],
+            fontSize: 14.0,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.grey[300],
+            letterSpacing: 1.0,
+          ),
+        ),
+      ),
+      home: Shell(),
+    );
+  }
+}
+
+class Shell extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                SideMenu(),
+                const Expanded(
+                    child: PlaylistScreen(playlist: lofihiphopPlaylist)),
+              ],
+            ),
+          ),
+          Container(
+            height: 84.0,
+            width: double.infinity,
+            color: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
+}
